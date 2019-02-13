c5dda6f9f5c9406a025206dda67363a2c3f5ed36
stepanVrtek <stepan.vrtek@gmail.com>
2019-02-13 11:48:29 +0100

    python-like range method

--- assets/js/prototype.js
+++ assets/js/prototype.js
@@ -4,7 +4,7 @@ Array.prototype.sum = function (prop) {
       total += this[i][prop]
   }
   return total
-}
+};
 
 Array.prototype.max = function (prop) {
   var maximum = 0
@@ -12,4 +12,27 @@ Array.prototype.max = function (prop) {
       if (this[i][prop] > maximum) maximum = this[i][prop];
   }
   return maximum
-}
+};
+
+function range(start, stop, step) {
+    if (typeof stop == 'undefined') {
+        // one param defined
+        stop = start;
+        start = 0;
+    }
+
+    if (typeof step == 'undefined') {
+        step = 1;
+    }
+
+    if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+        return [];
+    }
+
+    var result = [];
+    for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+        result.push(i);
+    }
+
+    return result;
+};
